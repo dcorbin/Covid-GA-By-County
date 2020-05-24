ce098215c769599b124173def9820f43a070aec1
David Corbin <dave@dcorbin.com>
2020-05-24 15:41:20 -0400

    Ready for production

---
+++ production/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -x
+eval "$(rbenv init -)"
+date >&2
+date
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}
+ruby fetch.rb
+ruby -I ${DIR}/lib post_fetch_processor.rb
