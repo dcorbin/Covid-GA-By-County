d6fcb4c416820aa12e4a3a99053a5b2f04db5f46
David Corbin <dave@dcorbin.com>
2020-05-24 15:58:34 -0400

    Fixed run.sh to ensure correct directory (really)

--- production/run.sh
+++ production/run.sh
@@ -5,7 +5,7 @@ eval "$(rbenv init -)"
 date >&2
 date
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ${DIR}/..
+DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
+cd ${DIR}
 ruby fetch.rb
-ruby -I ${DIR}/lib post_fetch_processor.rb
+ruby -I ./lib post_fetch_processor.rb
