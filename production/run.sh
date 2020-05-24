f115d4e54d6ff0045f407cc5d17d81b75b27535e
David Corbin <dave@dcorbin.com>
2020-05-24 17:04:09 -0400

    Copy tabulated data to deployed site.

--- production/run.sh
+++ production/run.sh
@@ -9,4 +9,5 @@ DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd 
 cd "${DIR}"
 ruby fetch.rb
 ruby -I ./lib post_fetch_processor.rb
+cp "${DATA_DIR}"/table.json /var/www/html/GA-By-County.json
 echo "Execution Complete" >&2
