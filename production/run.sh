f3688bb4a0906de34593be942cb65cd4cbd7c4d8
David Corbin <dave@dcorbin.com>
2020-05-24 16:15:19 -0400

    Minor cleanup to run script.sh

--- production/run.sh
+++ production/run.sh
@@ -5,7 +5,8 @@ eval "$(rbenv init -)"
 date >&2
 date
 
-DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
-cd ${DIR}
+DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
+cd "${DIR}"
 ruby fetch.rb
 ruby -I ./lib post_fetch_processor.rb
+echo "Execution Complete" >&2
